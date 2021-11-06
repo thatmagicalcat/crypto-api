1aaf73e7dae9e76dfc7ff21ea67c95fab33de2ac
Pranjal-Patel <85670037+Pranjal-Patel@users.noreply.github.com>
2021-11-06 12:13:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cheerio = require('cheerio')
 const axios = require('axios')
 
-const PORT = 6969
+const PORT = process.env.PORT || 6969
 const app = express()
 
 let quote = {}
